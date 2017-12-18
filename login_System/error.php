9bfaea26f378bee9212728311bf0d4b16f48948f
SaudAlqahtani <king77saud@gmail.com>
2017-12-18 15:03:26 +0300

    edited login and error buttons

--- login_System/error.php
+++ login_System/error.php
@@ -16,11 +16,11 @@ session_start();
     if( isset($_SESSION['message']) AND !empty($_SESSION['message']) ): 
         echo $_SESSION['message'];    
     else:
-        header( "location: index.php" );
+        header( "location: login_system/index.php" );
     endif;
     ?>
     </p>     
-    <a href="http://localhost:63342/test2/DiverstyShop%20front%20end.html"><button class="button button-block"/>Home</button></a>
+    <a href="index.php"><button class="button button-block"/>Home</button></a>
 </div>
 </body>
 </html>
